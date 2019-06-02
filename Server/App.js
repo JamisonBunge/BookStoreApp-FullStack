90320466ae06aca39c71e4ed132bb54ba1f86f5c
Jamison Bunge <JamisonBunge@outlook.com>
2019-06-02 13:56:40 -0400

    stable build

--- Server/App.js
+++ Server/App.js
@@ -1,29 +1,29 @@
-const express = require('express')
-const graphqlHTTP = require('express-graphql')
-const  schema = require('./schema/schema.js')
-const mongoose = require('mongoose')
-const app = express();
-const cors = require('cors');
+//IMPORTS
+const express = require('express')                  //express nodejs framework
+const graphqlHTTP = require('express-graphql')      //make express understand graphql
+const schema = require('./schema/schema.js')        //defines graph scehma for gql
+const mongoose = require('mongoose')                //connecting to remote mongoDB
+const cors = require('cors');                       //
 
+//init the app
+const app = express();
 
 //allow cross-origin requres
 app.use(cors());
 
-mongoose.connect('mongodb+srv://admin:thisismypassword@cluster0-hiujx.mongodb.net/test'); 
-mongoose.connection.once('open',() => {
+//connect to the databse
+mongoose.connect('mongodb+srv://admin:thisismypassword@cluster0-hiujx.mongodb.net/test');
+mongoose.connection.once('open', () => {
     console.log('connected to the DB');
-}); 
-
-
+});
 
 //set up middlewhwere
-
-app.use('/graphql',graphqlHTTP({
-    //empty object, to pass perams
+app.use('/graphql', graphqlHTTP({
+    //on the route of /graphql, the funciton graphqlHTTP is called to handle the request
     schema,
     graphiql: true
 }));
-//on the route of /graphql, the funciton graphqlHTTP is called to handle the request
 
 
-app.listen(4000, () => {console.log('listening for requests at port  4000')})
+
+app.listen(4000, () => { console.log('listening for requests at port  4000') })
